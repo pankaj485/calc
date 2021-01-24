c2f955317f37790719c55889c2e6c8d9a53d5c03
pankaj485 <61234787+pankaj485@users.noreply.github.com>
2021-01-24 17:01:17 +0530

    added calculation functionality
    added basic calcultaion functionality , changed the background color of AC button on light mode

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-/* 
-    lightImages 
+/*      
     lightBody 
-============================
-	lightThemeImage
-	darkThemeImage
-	lightCalculator	
-	lightHeading 
-	lightResult 
-	lightButtons
-	lightOperationButtons
-	lightResultButton
+    lightThemeImage
+    darkThemeImage
+    lightCalculator	
+    lightHeading 
+    lightResult 
+    lightButtons
+    lightOperationButtons
 */
+
+/* =====================================
+========= THEME TOOGLE FUNCTION ========
+======================================== */
 
 document.querySelector('.themeImage').addEventListener('click', () => {
 	document.querySelector('.lightThemeImage').classList.toggle('hideIt');
@@ -27,3 +28,29 @@ document.querySelector('.themeImage').addEventListener('click', () => {
 	}
 	document.querySelector('.resultButton').classList.toggle('lightResultButton');
 });
+
+/* ======================================
+========= CALCULATOR FUNCTIONALITY =======
+======================================== */
+
+let screen = document.getElementById('resultScreen');
+let buttons = document.querySelectorAll('button');
+let outputValue = 0;
+for (item of buttons) {
+	item.addEventListener('click', (event) => {
+		let buttonValue = event.target.innerHTML;
+		// console.log(`button value : ${buttonValue}`);
+		screen.innerHTML += buttonValue;
+
+		if (buttonValue === 'AC') {
+			outputValue = 0;
+			screen.innerHTML = '&nbsp;';
+		} else if (buttonValue === '=') {
+			// prints output
+			screen.innerHTML = eval(outputValue);
+		} else {
+			// updating the to evaluate things
+			outputValue += buttonValue;
+		}
+	});
+}
